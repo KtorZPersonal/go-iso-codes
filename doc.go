55a7426e6335579781a0dfc49c6bed9509a8239f
KtorZ <matthias.benkort@gmail.com>
2016-09-25 01:17:27 +0200

    add top-level package documentation

---
+++ doc.go
@@ -0,0 +1,12 @@
+// Copyright © 2016 Matthias Benkort
+// Use of this source code is governed by the MPL 2.0 license that can be found in the LICENSE file.
+
+// codes aggregates data to get various ISO code and details about countries, currencies and
+// languages among the world.
+//
+// The package offers methods to quickly lookup a country, language or currency based on a code, a
+// fraction of the name or a combination of other cirterias.
+//
+// The full lists of data are also available and can be easily generated to JSON via the
+// `encoding/json` package.
+package codes
