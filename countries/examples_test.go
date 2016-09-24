1083d50074dcd7eae3deb9a208fc78f6e70d16f0
KtorZ <matthias.benkort@gmail.com>
2016-09-25 01:12:05 +0200

    countries | add example for lookup()

---
+++ countries/examples_test.go
@@ -0,0 +1,49 @@
+// Copyright © 2016 Matthias Benkort
+// Use of this source code is governed by the MPL 2.0 license that can be found in the LICENSE file.
+
+package countries
+
+import (
+	"fmt"
+
+	"github.com/KtorZ/codes/currencies"
+	"github.com/KtorZ/codes/languages"
+)
+
+// sort ascendently sort a slice of countries on the Alpha3
+func sort(xs []Country) []Country {
+	var _xs []Country
+sort:
+	for _, a := range xs {
+		for i, b := range _xs {
+			if a.Alpha3 < b.Alpha3 {
+				_xs = append(_xs[:i], append([]Country{a}, _xs[i:]...)...)
+				continue sort
+			}
+		}
+
+		_xs = append(_xs, a)
+	}
+
+	return _xs
+}
+
+// ExampleLookup illustrates several lookups
+func ExampleLookup() {
+	results := Lookup(Country{Alpha3: "NLD"})
+	fmt.Println(results)
+
+	results = Lookup(Country{
+		Languages: []languages.Language{
+			languages.Language{Alpha3: "nld"},
+		},
+		Currencies: []currencies.Currency{
+			currencies.Currency{Alpha3: "EUR"},
+		},
+	})
+	fmt.Println(sort(results))
+
+	// Output:
+	// [{NL NLD 528 [{EUR 978 2 Euro}] [+31] [{nl nld Dutch}] Netherlands}]
+	// [{BE BEL 056 [{EUR 978 2 Euro}] [+32] [{nl nld Dutch} {fr fra French} {de deu German}] Belgium} {NL NLD 528 [{EUR 978 2 Euro}] [+31] [{nl nld Dutch}] Netherlands}]
+}
